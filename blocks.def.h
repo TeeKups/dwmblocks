a3dfb9e2178b7c5ab513cba8d594fc3fc8ed3b4a
Juhani Kupiainen <juhani@jkupiainen.fi>
2024-09-14 18:01:31 +0300

    Convert to CMake + cleanup

--- blocks.def.h
+++
@@ -1,9 +0,0 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
-static const Block blocks[] = {
-	/*Icon*/        /*Command*/                /*Update Interval*/    /*Update Signal*/
-	{"",            "date +'%A %d.%m.%Y [%W] %H:%M:%S'",                1,                     10},
-};
-
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
